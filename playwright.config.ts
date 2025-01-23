37f1b01b4b81c4ef486a414ef63a17ce2403982d
Thomas Pham <phamtomt@gmail.com>
2025-01-22 20:06:56 -0800

    Update GitHub Actions for uploading test results and videos

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['junit', { outputFile: 'results/test-results.xml' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -33,6 +37,7 @@ export default defineConfig({
     
     /* Custom test ID attribute */
     testIdAttribute: 'data-test',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
